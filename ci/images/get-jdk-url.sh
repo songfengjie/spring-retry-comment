18bb11a9f079d7c77851f3095ad305909b1f9b9d
songfengjie <morningsong@shein.com>
2023-11-14 20:31:10 +0800

    pom文件

---
+++ ci/images/get-jdk-url.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+set -e
+
+case "$1" in
+	java17)
+		echo "https://github.com/bell-sw/Liberica/releases/download/17.0.7+7/bellsoft-jdk17.0.7+7-linux-amd64.tar.gz"
+	;;
+	*)
+		echo $"Unknown java version"
+		exit 1
+esac
